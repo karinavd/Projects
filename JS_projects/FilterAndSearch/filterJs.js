0f518169afa978543b3a732b9e8f9939694a0ef5
Karina <karina2006.ka@gmail.com>
2025-07-13 15:05:54 +0300

    added project

---
+++ JS_projects/FilterAndSearch/filterJs.js
@@ -0,0 +1,228 @@
+let products = {
+    data:[
+        {
+productName: "Mixed berry mousse",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu-8.jpg",
+price:"45",
+        },
+         {
+productName: "Mango and coconut soufflé",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu-4.jpg",
+price:"30",
+        },
+         {
+productName: "Homemade carrot cake",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu-9.jpg",
+price:"35",
+        },
+         {
+productName: "Matcha cake",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu-10.jpg",
+price:"55",
+        },
+         {
+productName: "Vegan chocolate cake",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu-6.jpg",
+price:"60",
+        },
+         {
+productName: "Praline and ganache cake",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu-5.jpg",
+price:"55",
+        },
+         {
+productName: "Blondie with vanilla ice cream",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu-7.jpg",
+price:"35",
+        },
+         {
+productName: "Re-invented cheesecake",
+category:"Dessert",
+image:"https://www.theforkmanager.com/hubfs/Imported_Blog_Media/the-fork-10-desserts-ideas-to-add-to-your-menu.jpg",
+price:"40",
+        },
+         {
+productName: "Crab Salad, Cucumber, Avocado Puree & Citrus Dressing",
+category:"FirstCourses",
+image:"https://temptationforfood.com/wp-content/uploads/2018/10/CrabSaladAvocadoPuree-07609@NewWebsmlSQ-768x1153.jpg",
+price:"150",
+        },
+         {
+productName: "Tuna Sashimi, Ruby Grapefruit & Ponzu Dressing",
+category:"FirstCourses",
+image:"https://temptationforfood.com/wp-content/uploads/2019/09/TunaCev-7188@sml4SQ.jpg",
+price:"200",
+        },
+        {
+productName: "Buffalo Haloumi, Herb Salad, Pomegranate & Lemon",
+category:"FirstCourses",
+image:"https://temptationforfood.com/wp-content/uploads/2019/04/Haloumi-5848@NewWebsmlSQ.jpg",
+price:"130",
+        },
+        {
+productName: "Squid Salad, Roast Tomato, Potato & Capsicum Chilli Relish",
+category:"FirstCourses",
+image:"https://temptationforfood.com/wp-content/uploads/2019/02/SquidSal-5197@sml4SQ-768x1080.jpg",
+price:"170",
+        },
+        {
+productName: "Prawn, Pancetta Linguini, Tomato, Chilli & Pangrattato",
+category:"FirstCourses",
+image:"https://temptationforfood.com/wp-content/uploads/2018/10/IMG_1718@NewWebsmlSQ.jpg",
+price:"250",
+        },
+          {
+productName: "Meat and vegetables",
+category:"SecondCourses",
+image:"https://i.pinimg.com/736x/42/0a/84/420a84ffce30411bdb87f697b6a48727.jpg",
+price:"300",
+        },
+        {
+productName: "California Chicken Salad",
+category:"SecondCourses",
+image:"https://i.pinimg.com/736x/3c/66/f3/3c66f3a18805275e416d0a170fd174cc.jpg",
+price:"200",
+        },{
+productName: "Light & Flavorful Healthy Chicken Salad",
+category:"SecondCourses",
+image:"https://i.pinimg.com/736x/43/32/49/43324901330e4028f87b3ae97bf1971f.jpg",
+price:"200",
+        },{
+productName: "Salmon Salad",
+category:"SecondCourses",
+image:"https://i.pinimg.com/736x/03/02/77/030277c8585450b894a5cdb712771ebc.jpg",
+price:"250",
+        },{
+productName: "Chicken Pesto Pasta Salad",
+category:"SecondCourses",
+image:"https://i.pinimg.com/736x/a2/8f/4d/a28f4db1f686e51c8431435e768d3661.jpg",
+price:"200",
+        },{
+productName: "Iced London Fog",
+category:"Drinks",
+image:"https://www.allrecipes.com/thmb/OcnaPn6qzc2-L88sOkmaVNyjrHQ=/364x242/filters:no_upscale():max_bytes(150000):strip_icc():focal(1999x0:2001x2):format(webp)/11752200_Iced-London-Fog_YolandaGutierrez_4x3-13f57d1e6abe4b1bb14a6e053b0b5d6b.jpg",
+price:"50",
+        },{
+productName: "Red, White, and Blueberry Lemonade",
+category:"Drinks",
+image:"https://www.allrecipes.com/thmb/QhwVDT_RognHjLKX0Oh7UjUEvFc=/364x242/filters:no_upscale():max_bytes(150000):strip_icc():focal(1999x0:2001x2):format(webp)/11753759_Red-White-and-Blueberry-Lemonade_John-Mitzewich_4x3-71be72a91b5e4ed49e7e76a23c97e573.jpg",
+price:"55",
+        },{
+productName: "Freddo Espresso",
+category:"Drinks",
+image:"https://www.allrecipes.com/thmb/TwbPVY772ic05F4Beh0ePm01r80=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():focal(2048x1382:2050x1384):format(webp)/11739755-freddo-espresso-recipe-VAT-003-4x3-bb88913d2cf54fd8b3e7fd1c8153de74.jpg",
+price:"60",
+        },{
+productName: "Strawberry Matcha Latte",
+category:"Drinks",
+image:"https://www.allrecipes.com/thmb/JDX8SUeiEJVrT26FFezbpjmLXXU=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():focal(1999x0:2001x2):format(webp)/11747561StrawberryMatchaLattePatBernitt4x3-47a554208775495bb8d640ea726358f2.jpg",
+price:"60",
+        },{
+productName: "Juneteenth 'Red Punch' Strawberry Lemonade",
+category:"Drinks",
+image:"https://www.allrecipes.com/thmb/c6TIccQdB5CSKYYDwmC0Uoo63lE=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():focal(2033x1533:2035x1535):format(webp)/11738621-juneteenth-red-punch-strawberry-lemonade-recipe-11738621-VAT-001-4x3-3cd728dea76e4a1f95fc9e59a02c5491.jpg",
+price:"55",
+        },{
+productName: "Buffalo Cauliflower Wings",
+category:"Appetizer",
+image:"https://cdn.loveandlemons.com/wp-content/uploads/2022/10/buffalo-cauliflower-recipe.jpg",
+price:"100",
+        },{
+productName: "Stuffed Mushrooms",
+category:"Appetizer",
+image:"https://cdn.loveandlemons.com/wp-content/uploads/2019/12/stuffed-mushrooms-1.jpg",
+price:"120",
+        },{
+productName: "Taquitos",
+category:"Appetizer",
+image:"https://cdn.loveandlemons.com/wp-content/uploads/2020/01/taquitos-recipe.jpg",
+price:"130",
+        },{
+productName: "Fresh Spring Rolls",
+category:"Appetizer",
+image:"https://cdn.loveandlemons.com/wp-content/uploads/2023/05/spring-roll.jpg",
+price:"140",
+        },{
+productName: "Quesadillas",
+category:"Appetizer",
+image:"https://cdn.loveandlemons.com/wp-content/uploads/2024/01/quesadilla-recipe.jpg",
+price:"130",
+        },
+]
+}
+
+for(let item of products.data){
+    let card  = document.createElement('div');
+    card.classList.add('card',item.category.toLowerCase(),'hide');
+    let imgContainer = document.createElement('div');
+    imgContainer.classList.add("imgContainer");
+    let image = document.createElement('img');
+    image.setAttribute('src',item.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    let container = document.createElement('div');
+    container.classList.add('container');
+    let name = document.createElement('h5');
+    name.classList.add("nameProduct");
+    name.innerText = item.productName;
+    container.appendChild(name);
+
+ let price = document.createElement('h6');
+    price.classList.add("priceProduct");
+    price.innerText = "$" + item.price;
+    container.appendChild(price);
+
+    card.appendChild(container)
+    document.getElementById('items').appendChild(card);
+}
+function filterItems(value){
+let buttons = document.querySelectorAll('.btnAll');
+buttons.forEach((x)=>{
+        if(value.toLowerCase()==x.innerText.toLowerCase().replace(/\s/g, '')){
+                x.classList.add('active')
+        }
+        else{
+               x.classList.remove('active')
+        }
+        });
+        let elements = document.querySelectorAll('.card')
+        elements.forEach(e=>{
+                if(value=="all"){
+                    e.classList.remove("hide");    
+                }
+                else{
+                        if(e.classList.contains(value.toLowerCase())){
+                                   e.classList.remove("hide");     
+                        }
+                        else{
+                                e.classList.add("hide");    
+                        }
+                }
+        })
+
+}
+
+document.getElementById('searchButton').addEventListener("click",()=>{
+        let search=document.getElementById("searchInput").value;
+        let elements = document.querySelectorAll('.nameProduct');
+        let cards= document.querySelectorAll('.card');
+        elements.forEach((e,index)=>{
+if(e.innerText.toLowerCase().includes(search.toLowerCase())){
+        cards[index].classList.remove('hide')
+}
+else{
+         cards[index].classList.add('hide')
+}
+        })
+})
+
+window.onload=()=>{
+        filterItems("all");
+}
